eaa8cd263dbdc936d0803b4ff4f76c4fcc7223c1
adamxz <adamxz@126.com>
2016-06-29 00:11:31 +0800

    daily_price

--- src/dailyprice_create_sql.py
+++ src/dailyprice_create_sql.py
@@ -22,7 +22,12 @@ import pymysql
 
 import mysql
 
-
+db_host = 'localhost'
+db_user = 'root'
+db_passwd = '1234'
+db_name = 'securities_master'
+charset_type = 'utf8'
+table_name = 'daily_price'
 table_column_num = 10
 
 price_url = 'http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_FuQuanMarketHistory/\
@@ -67,30 +72,33 @@ def get_id_security(db_host, db_user, db_passwd, db_name):
         for id in id_security_tmp:
             id = str(id)
             id_security.append(id[4:10])
+        cursor.close()
+        conn.close()
     return id_security
         
 # Analyse the soup
 # body-div-div id="main"-div id="center"
 def soup_analyze(soup):
-    soup.div.get
+    pass
 
 if __name__ == '__main__':
-    sql = mysql.Mysql()
+    id_securities = get_id_security(db_host, db_user, db_passwd, db_name)
+    sql = mysql.Mysql(db_host, db_user, db_passwd, db_name, charset_type)
     sql.open_conn()
-    id_security = 300001
-    soup = soup_read(id_security, year_today,quarter_today)
-    soup_table = soup.find_all(align = 'center') #从下标1开始，每8个一组数据。
-    year_start = soup_table[0].find_all('option')[-5].string  # 获得年份
-    year_range = range(year_today,int(year_start),-1)
-    insert_columns = 'id_security, date, price_open, price_high, price_close, price_low, volumn, amount, factor_adj'
-    for year in year_range:
+    
+    for id_security in id_securities:
         soup = soup_read(id_security, year_today,quarter_today)
         soup_table = soup.find_all(align = 'center') #从下标1开始，每8个一组数据。
-        for i in range(9,len(soup_table),8):
-            insert_item = []
-            insert_item.append(soup_table[i].a.string[7:17])
-            for j in range(1,8):
-                insert_item.append(soup_table[i+j].string)
-            sql.insert(insert_item, )
-    
-    print(soup)
+        year_start = soup_table[0].find_all('option')[-5].string  # 获得年份
+        year_range = range(year_today,int(year_start),-1)
+        insert_columns = 'id_security, date, price_open, price_high, price_close, price_low, volumn, amount, factor_adj'
+        for year in year_range:
+            soup = soup_read(id_security, year_today,quarter_today)
+            soup_table = soup.find_all(align = 'center') #从下标1开始，每8个一组数据。      
+            for i in range(9,len(soup_table),8):
+                insert_item = "'" + str(id_security) + "', "
+                insert_item = insert_item + "'" + soup_table[i].a.string[7:17] + "'"
+                for j in range(1,8):
+                    insert_item = insert_item + ", '"+ soup_table[i+j].string + "'"
+                    sql.insert(table_name, insert_columns, insert_item)
+    sql.close_conn()
---
+++ src/mysql.py
@@ -0,0 +1,52 @@
+'''
+Created on Jun 26, 2016
+
+@author: adam
+'''
+
+import pymysql
+
+class Mysql(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, db_host = 'localhost', db_user = 'root', db_passwd = '1234',\
+                 db_name = 'securities_master', charset_type = 'utf8'):
+        '''
+        Constructor
+        '''
+        self.db_host = db_host 
+        self.db_user = db_user
+        self.db_passwd = db_passwd
+        self.db_name = db_name
+        self.charset_type = charset_type 
+        
+    
+    def open_conn(self):
+        try:
+            self.conn = pymysql.connect(host = self.db_host, user = self.db_user, passwd = self.db_passwd,\
+                                   db = self.db_name,charset = self.charset_type)
+        except Exception:
+            print(Exception.args[0])
+        else:
+            self.cursor = self.conn.cursor()
+            
+    
+    def insert(self, table_name, insert_columns, insert_values):
+        # column_value_str = ('%s, ' * column_num)[:-2]
+        insert_str = "insert into %s (%s) values (%s)" %(table_name, insert_columns, insert_values)
+        self.cursor.execute(insert_str)
+        
+    def select(self, select_columns, table_name):
+        select_str = 'select (%s) from %s' %(select_columns, table_name)
+        self.cursor.execute(select_str)
+        
+    def close_conn(self):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+        
+if __name__ == '__main__':
+    pass
